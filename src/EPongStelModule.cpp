c6e84442ad0bc87dfd643b21dd72dcd0f5e4db56
Peter Reineke <preineke@gmx.com>
2010-09-02 18:02:15 +0200

    added moving balls

--- src/EPongStelModule.cpp
+++ src/EPongStelModule.cpp
@@ -25,6 +25,22 @@
 #include "EPongStelModule.hpp"
 
 #include <QDebug>
+#define BALLS 100 // better divisble by 4
+
+Vec3f ballPos[BALLS];
+Vec3f ballNormal[BALLS];
+char ballEvent[BALLS];
+double eventTime[BALLS];
+
+static double lastTime; 
+enum {
+	BALL_CREATE,
+	BALL_BOUNCE,
+	BALL_PADDLE
+};
+#define PI 3.14
+#define HTT 5 
+// Time in seconds a ball needs to make a halfturn
 
 /*************************************************************************
  This method is the one called automatically by the StelModuleMgr just 
@@ -82,6 +98,36 @@ double EPongStelModule::getCallOrder(StelModuleActionName actionName) const
 void EPongStelModule::init()
 {
 	qDebug() << "init called for EPongStelModule";
+	//time = 0;
+	int i;
+	for(i = 0; i < BALLS; i++)
+	{
+		ballEvent[0] = BALL_CREATE;	
+		ballPos[i].set(0.0f, 0.0f, 1.0f);
+		ballNormal[i].set(sin(2.0f * PI * i/BALLS), 
+			cos(2.0f * PI * i / BALLS), 0.0f);
+	}
+}
+
+inline void rotate(Vec3f &pos, Vec3f &normal, float alpha)
+{
+	float x,y,z;
+	float s,c,t;
+	s = sin(alpha);
+	c = cos(alpha);
+	printf("s = %f, c = %f\n", s,c);	
+	t = 1.0f - c;
+	x = pos[0] * (t * normal[0] * normal[0] + c) +
+		pos[1] * (t * normal[0] * normal[1] - s * normal[2]) +
+		pos[2] * (t * normal[0] * normal[2] + s * normal[1]);
+	y = pos[0] * (t * normal[0] * normal[2] + s * normal[2]) +
+		pos[1] * (t * normal[1] * normal[1] + c) +
+		pos[2] * (t * normal[1] * normal[2] - s * normal[0]);
+	z = pos[0] * (t * normal[0] * normal[2] - s * normal[1]) +
+		pos[1] * (t * normal[1] * normal[2] + s * normal[0]) +
+		pos[2] * (t * normal[2] * normal[2] + c);
+	pos.set(x,y,z);
+	printf("x = %f, y = %f, z = %f\n",pos[0], pos[1], pos[2]); 
 }
 
 /*************************************************************************
@@ -89,9 +135,16 @@ void EPongStelModule::init()
 *************************************************************************/
 void EPongStelModule::draw(StelCore* core)
 {
-	StelPainter painter(core->getProjection(StelCore::FrameAltAz));
+	Vec3f xy;
+	StelProjectorP prj = core->getProjection(StelCore::FrameAltAz);
+	StelPainter painter(prj);
 	painter.setColor(1,1,1,1);
 	painter.setFont(font);
-	painter.drawText(00, 00, "EPong World!");
+	int i;
+	for (i = 0; i<BALLS; i++) {
+		if (ballEvent[i] != BALL_CREATE) rotate(ballPos[i], ballNormal[i], 0.05f);	
+		prj->project(ballPos[i], xy);
+		painter.drawCircle(xy[0], xy[1], 10); //qDebug() << "end of drawing epong";
+	}
 }
 
